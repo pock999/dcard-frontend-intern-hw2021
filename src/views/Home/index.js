1044cf49916a6e298c53583511e117668ef765d9
Rickh <hpx210650@gmail.com>
2021-03-08 03:39:45 +0800

    update

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Home = () => (
   <>
     <h1>
       Home
     </h1>
+    <Link to='/scenicSpot'>景點</Link>
   </>
 );
 
