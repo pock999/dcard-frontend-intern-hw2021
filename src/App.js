2cf0713bfca471e90265c0e8c0cbd3c3453953dd
Rickh <hpx210650@gmail.com>
2021-04-12 23:33:50 +0800

    update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ import {
 
 global._ = _;
 
+if (process.env.NODE_ENV === 'development') {
+  global.cors = '';
+} else {
+  global.cors = 'https://cors-anywhere.herokuapp.com/';
+}
+
 function App() {
   return (
     <Routers />
